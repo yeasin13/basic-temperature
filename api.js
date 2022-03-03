e26a12b3ea67d686c9a8f089231afd369ffcb342
yeasin13 <yeasinarafat8001@gmail.com>
2022-03-03 23:19:02 +0600

    city-show

--- api.js
+++ api.js
@@ -2,5 +2,17 @@ const  API_KEY = `8ef4120dae76699c2161adb94019a2e6`
 const searchTemperature = () => {
     const city =  document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    console.log(url);
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
+}
+
+const setInnertext = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnertext('city',temperature.name);
+    console.log(temperature);
 }
