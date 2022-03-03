b760a4378885f4036cda0821826313376e86408d
yeasin13 <yeasinarafat8001@gmail.com>
2022-03-04 00:20:32 +0600

    icone-add

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const  API_KEY = `8ef4120dae76699c2161adb94019a2e6`
 const searchTemperature = () => {
     const city =  document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     
     fetch(url)
     .then(res => res.json())
@@ -14,5 +14,10 @@ const setInnertext = (id, text) => {
 
 const displayTemperature = temperature => {
     setInnertext('city',temperature.name);
-    console.log(temperature);
+    setInnertext('temperature', temperature.main.temp);
+    setInnertext('condition', temperature.weather[0].main);
+    //set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 }
